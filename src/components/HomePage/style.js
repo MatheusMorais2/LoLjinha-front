becb24acb31f9b3514ad44ee4addd4c92e903a8d
Luiza Santiago <lu.sntgo@gmail.com>
2022-02-12 09:31:59 -0300

    feat: add product page

--- src/components/HomePage/style.js
+++ src/components/HomePage/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const Container = styled.div`
-  min-height: 40vh;
+  min-height: 45vh;
   width: 60%;
   display: flex;
   justify-content: center;
@@ -40,55 +40,66 @@ const ItemBox = styled.div`
   display: flex;
   width: 140px;
   height: 180px;
-  overflow-wrap: break-word;
   text-align: center;
-  flex-direction: column;
-  margin: 30px;
+  margin: 10px;
   margin-left: 100px;
   background-color: #efe7da;
 
   border-radius: 8px;
-  justify-content: center;
-  align-items: center;
 
   img {
+    margin: 5px;
     width: 90px;
     height: 90px;
   }
   span {
-    width: 100px;
     overflow-wrap: break-word;
     font-family: Raleway;
     font-style: normal;
     font-weight: bold;
     font-size: 18px;
     line-height: 18px;
-    color: black;
-    margin: 5px;
+    color: #0c141d;
   }
 `;
 
 const ItemList = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
   width: 100%;
   border-radius: 10px;
 `;
-
+const Item = styled.div`
+  width: 140px;
+  height: 180px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 const Footer = styled.div`
   min-height: 40vh;
   width: 100%;
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-end;
 
   .poro-gif {
     width: 50px;
     height: 50px;
+    margin-bottom: 20px;
     background-image: url("http://pa1.narvii.com/6378/a79f542fe99748319baa292def19453ddc0d9f45_00.gif");
     background-size: 50px 50px;
   }
 `;
 
-export { Container, Header, StyledLink, Content, ItemBox, ItemList, Footer };
+export {
+  Container,
+  Header,
+  StyledLink,
+  Content,
+  ItemBox,
+  ItemList,
+  Footer,
+  Item,
+};
